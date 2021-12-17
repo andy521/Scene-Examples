d46b0d73e2a55e3d37eb0c3015afe5bd61245d3e
xucz <xgfd3@users.noreply.github.com>
2021-12-17 13:46:37 +0800

    add Live and uiwidget module

---
+++ Android/uiwidget/src/main/java/io/agora/uiwidget/function/PreviewControlView.java
@@ -0,0 +1,24 @@
+package io.agora.uiwidget.function;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class PreviewControlView extends BaseFunctionView{
+    public PreviewControlView(@NonNull Context context) {
+        super(context);
+    }
+
+    public PreviewControlView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PreviewControlView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+}
